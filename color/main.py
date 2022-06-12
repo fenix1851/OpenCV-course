914c38ffa65673d84e158ec90b1616c3981fbdb3
fenix1851 <foxyess@mail.ru>
2022-06-12 16:45:48 +0300

    working with color, replace simple kitten to cmonya

--- color/main.py
+++ color/main.py
@@ -3,7 +3,11 @@ import numpy
 
 img = cv2.imread("./src/img/image.jpeg")
 
-img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+r, g, b = cv2.split(img)
+ 
+img = cv2.merge([b,g,r])
 
 cv2.imshow("Res", img)
 cv2.waitKey(0)
